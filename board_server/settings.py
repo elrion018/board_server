66f62903bbc165836e82063dc3cf309fdce14cab
Daniel <elrion018@gmail.com>
2020-10-18 21:22:41 +0900

    edit setting.py

--- board_server/settings.py
+++ board_server/settings.py
@@ -135,9 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-#d
-
+# STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
